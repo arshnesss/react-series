c523cff7811a00abbdc3f490b8edeea90812cffa
arsh sharma <imarshsharma2401@gmail.com>
2025-04-10 19:33:31 +0530

    feat: initialize Tailwind CSS project with React and Vite
    - Added package.json with dependencies for React, Tailwind CSS, and Vite.
    - Included Vite logo SVG in the public directory.
    - Created App.css for styling with Tailwind CSS.
    - Developed App.jsx as the main application component with state management.
    - Added React logo SVG in assets.
    - Implemented Card component for displaying content.
    - Set up index.css for global styles and Tailwind CSS integration.
    - Configured main.jsx for rendering the application.
    - Established Vite configuration for React and Tailwind CSS plugins.

---
+++ 03tailwindprops/src/App.jsx
@@ -0,0 +1,32 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Card from './components/card'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      
+  
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      
+      <h1 className="text-3xl font-bold text-blue-600">
+      Hello Tailwind!
+      </h1>
+
+      <Card channel="chai aur code"  btn="yoo"/>
+    </>
+  )
+}
+
+export default App
